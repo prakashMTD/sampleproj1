82d9d137e01fc78a50388a2d7a13d93143546594
prakashMTD <prakashmt95@gmail.com>
2022-07-19 14:54:57 +0530

    fist push master

---
+++ src/org/emp/GitClass.java
@@ -0,0 +1,13 @@
+package org.emp;
+
+public class GitClass {
+	
+private void method1() {
+	System.out.println("Welcome java");
+}
+
+private void method2() {
+System.out.println("welcome selenium");
+}
+
+}
